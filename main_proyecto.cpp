2e14e9ebc8a4238975c73eeebbfbcf666f8a4c42
D4n7056 <A07107056@tec.mx>
2024-11-17 21:25:54 -0600

    main_proyecto.cpp

--- main_proyecto.cpp
+++ main_proyecto.cpp
@@ -19,13 +19,16 @@ int main(){
     paisaje.set_cantidad(3);
     paisaje.set_descripcion("dibujo de un paisaje");
     paisaje.set_tamano(1);
+    paisaje.set_material("lapices de colores");
+    paisaje.set_estilo("digital");
     paisaje.imprime_datos();
     paisaje.calcular_precio(3,1);
     Botones perro(1,3, "boton con imagen de perro","animales");
     perro.imprime_datos();
     perro.set_cantidad(2);
-    perro.set_descripcion("boton de un perro");
+    perro.set_descripcion("boton de un perro de felpa");
     perro.set_tamano(2);
+    perro.set_tema("juegetes");
     perro.imprime_datos();
     perro.calcular_precio(2,2);
     Pines planeta(1,1, "pin de saturno", "planetas");
@@ -33,6 +36,7 @@ int main(){
     planeta.set_cantidad(3);
     planeta.set_descripcion("pin de un planeta");
     planeta.set_tamano(1);
+    planeta.set_tema("universo");
     planeta.imprime_datos();
     planeta.calcular_precio(3,1);
 };
