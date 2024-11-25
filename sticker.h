174a561b616c8e3d7435603a62c0df2f4548f17e
D4n7056 <A07107056@tec.mx>
2024-11-24 20:29:33 -0600

    sticker.h
    implementación de herencia de para la clase stickers

--- sticker.h
+++ sticker.h
@@ -1,74 +1,30 @@
-#include <iostream>
-using namespace std;
-
 #ifndef STICKER_H
 #define STICKER_H
 
-class Sticker {
+#include <iostream>
+#include "producto.h"
+using namespace std;
+
+class Sticker : public Producto {
     private:
-        int cantidad; 
-        int tamano; 
-        string descripcion;
-    
+        string estilo;
+
     public: 
-        Sticker(){
-           cantidad = 5;
-           tamano = 1;
-           descripcion = "imagen impresa en papel adhesivo";
+        Sticker(): Producto() {
+            estilo = "tradicional";
         }
 
-        Sticker(int _cantidad, int _tamano, string _descripcion){
-            cantidad = _cantidad;
-            tamano = _tamano;
-            descripcion = _descripcion;
+        Sticker(string _estilo, int _cantidad, int _tamano, string _descripcion, float _precio_unitario_1, float _precio_unitario_2, float _precio_unitario_3 ) :  Producto( _cantidad, _tamano, _descripcion, _precio_unitario_1, _precio_unitario_2, _precio_unitario_3){
+            estilo = _estilo;
         }
 
-        void imprime_datos(){
-            cout << "CANTIDAD: " << cantidad << endl;
-            cout << "TAMAÑO: " << tamano << endl;
-            cout << "DESCRIPCION: " << descripcion << endl;
-            cout << "-----------------------\n";
-        }
-        void calcular_precio(int cantidad, int tamano){
-            float precio;
-            if (tamano = 1){
-                precio = 5*cantidad;
-            }
-            else{
-                if (tamano = 2){
-                    precio = 10*cantidad;
-                }
-                else{
-                    if (tamano = 3){
-                        precio = 15*cantidad;
-                    }
-                    else{
-                        cout << "valor no valido";
-                    }
-                }
-            }
-        cout << "PRECIO: " << precio << endl;
-        cout << "----------------------- \n";
+        string get_estilo(){
+            return estilo;
         }
 
-        int get_cantidad(){
-            return cantidad;
-        }
-        int get_tamano(){
-            return tamano;
-        }
-        string get_descripcion(){
-            return descripcion;
+        void set_estilo (string _estilo){
+            estilo = _estilo;
         }
 
-        void set_cantidad(int _cantidad){
-            cantidad = _cantidad;
-        }
-        void set_tamano(int _tamano){
-            tamano = _tamano;
-        }
-        void set_descripcion(string _descripcion){
-            descripcion = _descripcion;
-        }
 };
 #endif
