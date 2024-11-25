897c54921162f2929575eb9038e969aa7762cf13
D4n7056 <A07107056@tec.mx>
2024-11-24 20:27:19 -0600

    botones.h
    implementación de herencia de para la clase botones

--- botones.h
+++ botones.h
@@ -1,81 +1,26 @@
-#include <iostream>
-using namespace std;
-
 #ifndef BOTONES_H
 #define BOTONES_H
-class Botones {
+
+#include <iostream>
+#include "producto.h"
+using namespace std;
+
+class Botones : public Producto {
     private:
-        int cantidad; 
-        int tamano; 
-        string descripcion;
         string tema;
     
     public: 
-        Botones(){
-           cantidad = 5;
-           tamano = 1;
-           descripcion = "boton";
+        Botones() : Producto(){
            tema = "botanica";
         }
-        Botones(int _cantidad, int _tamano, string _descripcion, string _tema){
-            cantidad = _cantidad;
-            tamano = _tamano;
-            descripcion = _descripcion;
+        Botones(string _tema, int _cantidad, int _tamano, string _descripcion, float _precio_unitario_1, float _precio_unitario_2, float _precio_unitario_3 ) :  Producto( _cantidad, _tamano, _descripcion, _precio_unitario_1, _precio_unitario_2, _precio_unitario_3){
             tema = _tema;
         }
 
-        void imprime_datos(){
-            cout << "CANTIDAD: " << cantidad << endl;
-            cout << "TAMAÑO: " << tamano << endl;
-            cout << "DESCRIPCION: " << descripcion << endl;
-            cout << "TEMA: " << tema << endl;
-            cout << "-----------------------\n";
-        }
-
-        void calcular_precio(int cantidad, int tamano){
-            float precio;
-            if (tamano = 1){
-                precio = 15*cantidad;
-            }
-            else{
-                if (tamano = 2){
-                    precio = 25*cantidad;
-                }
-                else{
-                    if (tamano = 3){
-                        precio = 30*cantidad;
-                    }
-                    else{
-                        cout << "valor no valido";
-                    }
-                }
-            }
-        cout << "PRECIO: " << precio << endl;
-        cout << "----------------------- \n";
-        }
-
-        int get_cantidad(){
-            return cantidad;
-        }
-        int get_tamano(){
-            return tamano;
-        }
-        string get_descripcion(){
-            return descripcion;
-        }
         string get_tema(){
             return tema;
         }
 
-        void set_cantidad(int _cantidad){
-            cantidad = _cantidad;
-        }
-        void set_tamano(int _tamano){
-            tamano = _tamano;
-        }
-        void set_descripcion(string _descripcion){
-            descripcion = _descripcion;
-        }
         void set_tema(string _tema){
             tema = _tema;
         }
