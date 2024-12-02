ef9d9a6b275d08a94c6cf4cb9c7d91bae8f1928a
D4n7056 <A07107056@tec.mx>
2024-12-01 19:06:40 -0600

    botones.h

--- botones.h
+++ botones.h
@@ -1,28 +1,56 @@
+// alumno: Daniela Iliana Rivera García
+// matricula: A07107056
 #ifndef BOTONES_H
 #define BOTONES_H
 
 #include <iostream>
 #include "producto.h"
+
 using namespace std;
 
+// Clase Botones heredada de Producto
 class Botones : public Producto {
-    private:
-        string tema;
-    
-    public: 
-        Botones() : Producto(){
-           tema = "botanica";
-        }
-        Botones(string _tema, int _cantidad, int _tamano, string _descripcion, float _precio_unitario_1, float _precio_unitario_2, float _precio_unitario_3 ) :  Producto( _cantidad, _tamano, _descripcion, _precio_unitario_1, _precio_unitario_2, _precio_unitario_3){
-            tema = _tema;
-        }
+private:
+    // Variable privada
+    string temaBoton;
 
-        string get_tema(){
-            return tema;
-        }
+public:
+    // Constructor por defecto
+    Botones() : Producto() {
+        temaBoton = "Botanica";
+    }
 
-        void set_tema(string _tema){
-            tema = _tema;
-        }
+    /**
+     * Constructor con parámetros para inicializar un botón.
+     * tema es el tema relacionado con el botón.
+     * cantidad es la cantidad de botones.
+     * tamano para los tipos de tamaño del botón.
+     * descripcion es la descripción breve del botón.
+     * precioUnitario1 es el precio para el tamaño tipo 1.
+     * precioUnitario2 es el precio para el tamaño tipo 2.
+     * precioUnitario3 es el precio para el tamaño tipo 3.
+     */
+    Botones(string tema, int cantidad, int tamano, string descripcion, 
+            float precioUnitario1, float precioUnitario2, float precioUnitario3) 
+        : Producto(cantidad, tamano, descripcion, precioUnitario1, precioUnitario2, precioUnitario3) {
+        temaBoton = tema;
+    }
+
+    /**
+     * Obtiene el tema del botón.
+     * return tema del botón.
+     */
+    string getTema() {
+        return temaBoton;
+    }
+
+    /**
+     * Establece el tema del botón.
+     * return tema asignado.
+     */
+    void setTema(string tema) {
+        temaBoton = tema;
+    }
 };
+
 #endif
